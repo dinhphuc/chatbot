5cc5e002c096f3c98c5634c3dab9187538311425
sauanla <Phucnd.zit@gmail.com>
2019-09-12 22:37:37 +0700

    fix Active All Simsimi

--- bot.js
+++ bot.js
@@ -9,7 +9,27 @@ const simsimi = require('simsimi')({
 });
 
 var answeredThreads = {};
+var botStatusThreads = {};
 var isSimsimi = false;
+
+blockGroupChat = function (threadID){ 
+	var blockGroupIds = ["id gourup chat", "id gourup chat"]; 
+	if (blockGroupIds.find(x => x == threadID)) {
+		 console.error("block GroupId: " + threadID);
+		 return true;
+	}
+	return false;
+}
+
+blockUserChat = function (threadID){  
+	var blockUserIds = ["id user", "id user"];
+	if (blockUserIds.find(x => x == threadID)) {
+		 console.error("block ID: " + threadID);
+		 return true;
+	}
+	return false;
+}
+
 login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, api) => {
 
 	api.setOptions({
@@ -17,24 +37,28 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, 
 		logLevel: "silent",
 		updatePresence: false
 	});
-	if (err) return console.error(err);
-	api.listen(function callback(err, message) {
-		console.log(message.threadID);
+	if (err) return console.error(err); 
 
+	api.listen(function callback(err, message) {
+		console.log(message.threadID); 
 		//Simsimi
-		if (message.body == "bot") {
+		if (message.body == "bot"||message.body == "Bot") {
+			botStatusThreads[message.threadID] = true;
 			isSimsimi = true;
 			api.sendMessage("Đã bật chế độ nói chuyện với bot (gõ offbot để tắt). Bắt đầu nào!", message.threadID);
 			return console.log("On sim");
-		} else if (message.body == "offbot") {
+		} else if (message.body == "offbot"||message.body == "Offbot") {
 			isSimsimi = false;
+			botStatusThreads[message.threadID] = false;
 			api.sendMessage("Đã tắt chế độ nói chuyện với bot.", message.threadID);
 		}
 
-		if (isSimsimi) {
+		if (isSimsimi&&botStatusThreads.hasOwnProperty(message.threadID)) {
 			(async () => {
 				try{ 
-					const response = await simsimi(message.body);
+					if(blockGroupChat(message.threadID)){return;};
+					if(blockUserChat(message.threadID)){return;}; 
+					const response = await simsimi(message.body); 
 					api.sendMessage(response, message.threadID);
 				}catch{
 					api.sendMessage("Bot không hiểu bạn nói. Xin lỗi nha :(", message.threadID);
@@ -49,19 +73,13 @@ login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, 
 			// Tìm id ở đây https://findmyfbid.in/
 			// Thêm 1 người vào chỉ cần thêm dấu ,"ID người"
 			// Group cũng thế
-			var blockUserIds = ["100000901359006", "100039875484263"];
-			var blockGroupIds = ["2406280742789782", "2705897476091143"];
-			var crushId = "10002701265xx59";
-			var myLoveId = "100008xx2353680";
 
-
-			if (blockUserIds.find(x => x == message.threadID)) {
-				return console.error("block ID: " + message.threadID);
-			}
-
-			if (blockGroupIds.find(x => x == message.threadID)) {
-				return console.error("block GroupId: " + message.threadID);
-			}
+			if(blockGroupChat(message.threadID)){
+				return;
+			};
+			if(blockUserChat(message.threadID)){
+				return;
+			}; 
 
 			answeredThreads[message.threadID] = true;
 			api.sendMessage("Tin nhắn trả lời tự động.\n- Trả lời `bot` để nói chuyện đỡ buồn.", message.threadID);
